ca2d34341c5754a4cec1d1ffed1cdd711e352300
neon <neon@darkstar.example.net>
2023-02-17 08:33:53 -0300

    update

--- README.md
+++ README.md
@@ -1,2 +1,500 @@
 # bashed
 bash wrapper for the ed editor
+
+## What this does?
+It wraps the ed editor in bash functions, allowing it use directly on command line.
+
+## Dependencies:
+- ed;
+- tmux;
+- highlight;
+- xclip;
+
+## Configuration:
+Add to your ~/.bashrc:
+
+````
+    source /path/to/bashed.sh
+````
+
+### Optional, prompt:
+You can customize the prompt to show the file size, and current line.  For example, in ~/.bashrc:
+
+````
+function prompt {
+	local dir="\[\e[0;32m\][\W]\[\e[0m\]"
+	local return="\[\e[0;33m\]\$?\[\e[0m\]"
+	if [[ -n $fileline ]] && [[ -n $filesize ]]
+	then
+		local edprompt="\[\e[0;32m\][$fileline][$filesize]\[\e[0m\]"
+		PS1="$dir-$return-$edprompt-$ "
+	else
+		PS1="$dir-$return-$ "
+	fi
+}
+
+PROMPT_COMMAND=prompt
+````
+
+### Optional, tmux:
+You can customize tmux to show the file path in it's pane name.  In ~/.tmux.conf:
+
+````
+set -g pane-border-status top
+set -g pane-border-format "#T, #P, #{pane_current_command}"
+set-window-option -g pane-active-border-style "fg=green,bold"
+set-option -g display-panes-time 10000
+````
+
+## How to use it:
+### Opening files:
+````
+eo file
+````
+When opening files, it will verify if the same file is already opened in another tmux pane.  It it does, these pane will be focused, otherwise, the file will be opened in the current pane.
+
+### Closing files:
+````
+ek
+````
+
+### Adding line:
+#### Append:
+````
+ea "line of text."
+````
+
+The text will be appended after the current focused line.  Without arguments, a blank line will be added.
+
+#### Insert:
+````
+ei "line of text."
+````
+
+The text will be inserted in the current line - 1.
+
+### Deleting:
+#### Deleting line:
+````
+edel
+````
+
+#### Deleting range:
+````
+edel n
+````
+
+Where n is the end line.  The lines from the current line to n will be deleted.  n can also be specified in the format +n, like:
+
+````
+edel +n
+````
+
+That will delete the text from current line, to the text +n line.  Examples:
+
+````
+edel 2
+edel 5,10
+edel 10,+2
+````
+
+### Changing:
+#### Changing line:
+````
+ech "new text."
+````
+
+Will change the current line to "new text.".
+
+#### Changing range:
+````
+ech "new text." n
+````
+
+Will change the text from the current line, to the n line, to "new text.".  For example:
+
+````
+ech "new text." 5
+ech "changed." +2
+````
+
+### Copying:
+#### Copy line:
+````
+ey n
+````
+
+The current line will be copied to line n.
+
+#### Copy range:
+````
+ey a b
+````
+
+The current line, to b, will be copied to line n.  For example:
+
+````
+ey 1 5
+````
+
+Will copy the current line, to line 5, to the line 1.  The second argument (5, in last case), can be set using +, like +5.
+
+#### Yank:
+Lines can be "yanked" like follows:
+
+````
+ey ''
+````
+
+This will copy the current line to the variable $yank.
+
+#### Yank to X11 clipboard:
+````
+ey '' '' x
+````
+
+Will copy the current line to X11 clipboard, using xclip.  The second argument can be setted to copy a range, like:
+
+````
+ey '' 10 x
+ey '' +5 x
+````
+
+### Joining:
+#### Joining lines:
+````
+ej
+````
+
+Will join the current line with the next.  Can receive a an argument, either a number n, or +n, like:
+
+#### Joining range:
+````
+ej 2
+ej +2
+````
+
+### Moving:
+#### Moving line:
+````
+em 2
+````
+
+Will move the current line to line 2.
+
+#### Moving range:
+````
+em 2 10
+````
+
+Will move the current line, to the line 10, to line 2
+
+````
+em 2 +5
+````
+
+Will move the current line, plus the five subsequent lines, to the line 2.
+
+### Substitution:
+#### Substitution on current line:
+````
+esu "a" "b"
+````
+
+Can receive the g argument, like:
+
+````
+esu "a" "b" '' g
+````
+
+To replace every ocurrence.
+
+#### Substition on range:
+````
+esu "a" "b" n
+````
+
+Will substitute "a" for "b" from the current line, to the line n.  n can be setted with +, like:
+
+````
+esu "a" "b" +n
+````
+
+It can also receive the g argument:
+
+````
+esu "a" "b" n g
+````
+
+Examples:
+````
+esu "a" "b" 10
+esu "a" "b" +5
+esu "a" "b" +5 g
+````
+
+### Searching:
+The most common searching mechanism would be:
+
+````
+ef 'g/re/p'
+````
+
+This will return a list with all occurences of "re".  More details on how to navigate this list in the Natigation section.
+
+### Levels:
+On ed, visualizing how many tabs/spaces the current line identation have can be tedious.  In bashed, you can use el to see the current line tab level:
+
+````
+el
+````
+
+and els for the current line space level.  By level I mean: a line beginning with one tab is 1 level, 2 tabs, 2 level, etc.  The same for spaces.
+
+### Navigation:
+#### On the text:
+##### Forward:
+To move one line forward:
+
+````
+es
+es +
+````
+
+To move n lines forward, use +n.  For example, to move 5 lines forward:
+
+````
+es +5
+````
+
+You can use a literal n to move a page forward:
+
+````
+es n
+````
+
+##### Backward:
+To move one line backward:
+
+````
+es -
+````
+
+To move n lines backward, use -n.  For example:
+
+````
+es -5
+````
+
+You can use p to move a page backward:
+
+````
+es p
+````
+
+##### Specific location:
+For example, to go to the line 8:
+
+````
+es 8 
+````
+
+To the end of the file:
+
+````
+es $
+es G
+````
+
+To the beginning of the file:
+
+````
+es b
+es g
+````
+
+To show the current line:
+
+````
+es l
+es .
+````
+
+##### Searching forward:
+You can search forward using:
+
+````
+es '/re'
+````
+
+For example, to search a line beginning with a tab, and ending with 'then':
+
+````
+es '/^\tthen$'
+````
+
+You can use two search terms, to display from one to another, for example:
+
+````
+es '/\t^if,/^\tfi'
+````
+
+Will display the next if/fi pair of 1 tab level.
+
+##### Range:
+````
+es 1,10
+es +1,10
+es 1,+10
+es +1,+10
+````
+
+##### Quick visualizing:
+````
+es c
+````
+
+Will display from $pagesize/2 above the current line, to $pagesize/2 below the current line.
+
+##### Repeating:
+````
+es r
+````
+
+Will repeat the last es command.
+
+#### On the searching list:
+##### Going up on the list:
+````
+em u
+````
+
+##### Goind down on the list:
+````
+em d
+````
+
+##### Going to a item in the list:
+````
+es fn
+````
+
+Where n should be a number of an item of the search list.
+
+##### Showing the content in the middle:
+````
+em m
+````
+
+If the search list have, for example, 3 items, and you want to see what's in the middle of item 1, and item 2:
+
+````
+es f1
+es m
+````
+
+##### Display the list again:
+````
+es s
+````
+
+#### Disable syntax:
+To disable syntax for the next command:
+
+````
+editsyntax=n command
+````
+
+#### Disable line numbering:
+To disable line numbering for the next command:
+
+````
+edcmd=p command
+````
+
+### Versioning:
+#### Storing:
+````
+et
+````
+
+Will store a copy of the current file.
+
+#### Listing:
+````
+eu l
+eu list
+````
+
+Will show a list of the copies of the current file previously stored using et.
+
+#### Showing:
+##### With syntax:
+````
+eu show n
+eu es n
+````
+
+##### Plain:
+````
+eu print n
+eu p n
+````
+
+#### Diff:
+````
+eu diff n1 n2
+````
+
+Will present a diff from the file n1, and file n2 from the stored file list (eu l).
+
+#### Selecting:
+````
+eu n
+````
+
+Will substitute the current file with the file n from the stored file list (eu l)
+
+### Variables:
+#### filename:
+Contains the complete path to the file beign edited.
+
+#### fileline:
+Contains the current line number.
+
+#### filesize:
+Contains the size of the file being edited.
+
+#### edcmd:
+Contains the command that should be used by es.  It should be p, or n.
+
+#### pagesize:
+Size of a page.  Used by es n, es p, and es c.
+
+#### eslast:
+Contains the last command executed by es.
+
+#### eslastarg:
+Contains the last argument received by es.
+
+#### diffarg:
+Contains the diff options.  By default: "--color -c".
+
+#### Highlight:
+##### syntax:
+Contains the name of the syntax file used by highlight for the current file.
+
+##### editsyntax:
+Set if es should use syntax.  Should be y, or n.
+
+##### hitheme:
+Contains the theme name used by highlight.
+
+### Using e:
+Commands can be passed directly to ed, using e, like:
+
+````
+e "a\nA new line.\n.\nw"
+e "1,2d\nw"
+````
+
---
+++ bashed.sh
@@ -0,0 +1,701 @@
+#!/usr/bin/env bash
+
+# highlight
+himode="xterm256"
+#hitheme="neon"
+#hitheme="lucretia"
+hitheme="bluegreen"
+function hi { highlight "$1" --syntax "$2" -s $hitheme -O $himode; }
+
+# edit
+editreadlines="$HOME/.edit/readlines"
+edcmd="n"
+editsyntax="y"
+diffarg="--color -c"
+
+function editwindow {
+	local window=
+	local pane=
+	local session="$(tmux display-message -p '#S')"
+	[[ -n $3 ]] && session="$3"
+	for i in $(tmux lsp -s -t "$session:0" -F '#I::#D #T')
+	do
+		if [[ $i == $1 ]] && [[ -n $window ]] && [[ -n $pane ]]
+		then
+			tmux select-window -t "$session:$window"
+			tmux select-pane -t "$pane"
+			[[ -n $2 ]] && editarg "$2" "$session"
+			return 1
+		fi
+
+		window="${i/::*/}"
+		pane="${i/*::/}"
+	done
+
+	return 0
+}
+
+function edit {
+	[[ -n $2 ]] && local filename="$2"
+	[[ -z $filename ]] && return 0
+	[[ ${filename:0:1} != '/' ]] && filename="$PWD/$filename"
+	if [[ -n $1 ]]
+	then
+		result="$(echo -e "$1" | ed -s "$filename")"
+		echo "$result"
+		filesize="$(wc -l "$filename" | cut -d ' ' -f1)"
+	fi
+}
+
+function editread {
+	if [[ $1 != 0 ]] && [[ $2 != 0 ]] && [[ $3 != 0 ]]
+	then
+		local f="$3"
+		[[ ${f:0:1} != '/' ]] && f="$PWD/$f"
+		local lines="$(edit "${1},${2}p" "$f")"
+		mkdir -p "$HOME/.edit"
+		echo "$lines" > "$editreadlines"
+	fi
+
+	if [[ -n $4 ]] && [[ -f $editreadlines ]]
+	then
+		if [[ -n $5 ]]
+		then
+			local f="$5"
+			[[ ${f:0:1} != '/' ]] && f="$PWD/$f"
+			edit "${4}r $editreadlines\nw" "$f"
+		else
+			edit "${4}r $editreadlines\nw"
+		fi
+	fi
+}
+
+function editcmd {
+	[[ -n $4 ]] && local filename="$4"
+	[[ -z $filename ]] && return 1
+	[[ ${filename:0:1} != '/' ]] && filename="$PWD/$filename"
+	if [[ -n $1 ]] && [[ -n $2 ]]
+	then
+		local lines="$(sed -n "${1},${2}p" "$filename")"
+		editread $1 $2 "$filename"
+		cat "$editreadlines" | $3 > "$HOME/.edit/temp"
+		mv "$HOME/.edit/temp" "$editreadlines"
+		edit "${1},${2}d\nw"
+		editread 0 0 0 $(($1 - 1))		
+		#f="$(echo "$lines" | $3)"
+		#edit "$1,$2d\n$(( $1 - 1 ))a\n$f\n.\nw" "$filename"
+		editshow ${1},$2
+	fi
+}
+
+function editstore {
+	[[ -n $1  ]] && local filename="$1"
+	[[ -z $filename ]] && return 1
+	[[ ${filename:0:1} != '/' ]] && filename="$PWD/$filename"
+	local date="$(date +'%Y-%m-%d_%H-%M-%S')"
+	local dir="$HOME/.edit/$(dirname "$filename")"
+	mkdir -p "$dir"
+	cp "$filename" "$dir"
+	mv "$HOME/.edit/$filename" "$HOME/.edit/${filename}_${date}"
+}
+
+function editundo {
+	[[ -n $4  ]] && local filename="$4"
+	[[ -z $filename ]] && return 1
+	[[ ${filename:0:1} != '/' ]] && filename="$PWD/$filename"
+	local dir="$HOME/.edit/$(dirname "$filename")"
+	local files=()
+	if [[ -d "$dir" ]]
+	then
+		local n=1
+		shopt -s dotglob
+		for i in $dir/*
+		do
+			local version="/${i/*\/\//}"
+			local f="${version:0:${#filename}}"
+			if  [[ -f $i ]] && [[ "$filename" == "$f" ]]
+			then
+				files[$n]="$i"
+				n=$((n + 1))
+			fi
+		done
+
+		shopt -u dotglob
+	fi
+
+	[[ ${#files[@]} -eq 0 ]] && return 2
+	if [[ $1 == "l" ]] || [[ $1 == "list" ]]
+	then
+		local n=1
+		for i in ${files[@]}
+		do
+			local version="/${i/*\/\//}"
+			local f="${version:0:${#filename}}"
+			if  [[ -f $i ]] && [[ "$filename" == "$f" ]]
+			then
+				echo "$n - $version"
+				n=$((n + 1))
+			fi
+		done
+	elif [[ $1 == "-" ]] || [[ $2 == "delete" ]]
+	then
+		[[ -z $2 ]] && return 3
+		local head="$2"
+		local tail="$3"
+		[[ -z $3 ]] && tail="$2"
+		for ((i=$head; i<=$tail; ++i))
+		do
+			[[ -n ${files[$i]} ]] \
+				&& rm "${files[$i]}" \
+				|| echo "?"
+		done
+	elif [[ $1 == "diff" ]]
+	then
+		[[ -z $2 ]] && [[ -z $3 ]] && return 3
+		if [[ -f ${files[$2]} ]] && [[ -f ${files[$3]} ]]
+		then
+			diff $diffarg "${files[$2]}" "${files[$3]}" 
+		else
+			echo "?"
+		fi
+	elif [[ $1 == "es" ]] || [[ $1 == "show" ]]
+	then
+		[[ -z $2 ]] && return 3
+		[[ -f ${files[$2]} ]] && editshow a "${files[$2]}"
+	elif [[ $1 == "p" ]] || [[ $1 == "print" ]]
+	then
+		[[ -z $2 ]] && return 3
+		[[ -f ${files[$2]} ]] \
+			&& editsyntax=n edcmd=p editshow a "${files[$2]}"
+	elif [[ $1 =~ [0-9]+ ]]
+	then
+		[[ -f ${files[$1]} ]] \
+			&& cp "${files[$1]}" "$filename" \
+			|| echo "?"
+		filesize="$(wc -l "$filename" | cut -d ' ' -f1)"
+	else
+		echo "?"
+		return 4
+	fi
+}
+
+function editarg {
+	[[ -n $1 ]] && argument="$1"
+	[[ -z $argument ]] && return 1
+	local session="$(tmux display-message -p '#S')"
+	[[ -n $2 ]] && session="$2"
+	if [[ $argument =~ ^[0-9]+$ ]]
+	then
+		tmux send-keys -t "$session" "edit \"${argument}n\""
+	else
+		argument="${argument//\//\\/}"
+		argument="${argument//\*/\\*}"
+		tmux send-keys -t "$session" "edit \"/${argument}/n\""
+	fi
+
+	tmux send-keys -t "$session" Enter
+}
+
+function editsyntax {
+	local shebang="$(edit 1p "$1")"
+	[[ $shebang =~ \#\!.*\ ?(bash|sh) ]] && syntax=bash
+	[[ $shebang =~ \#\!.*\ ?lua ]] && syntax=lua
+	[[ $shebang =~ \#\!.*\ ?perl ]] && syntax=perl
+	[[ $shebang =~ \#\!.*\ ?ruby ]] && syntax=ruby
+	[[ $shebang =~ \#\!.*\ ?python ]] && syntax=python
+	[[ -n $syntax ]] && return
+	[[ $1 == $HOME/.bashrc ]] && syntax=bash 
+	[[ $1 =~ \.awk ]] && syntax=awk
+	[[ $1 =~ \.conf ]] && syntax=conf
+	[[ $1 =~ \.cmake ]] && syntax=cmake
+	[[ $1 =~ \.css ]] && syntax=css
+	[[ $1 =~ \.cpp ]] && syntax=c
+	[[ $1 =~ \.c ]] && syntax=c
+	[[ $1 =~ \.diff ]] && syntax=diff
+	[[ $1 == /etc/fstab ]] && syntax=fstab
+	[[ $1 =~ \.elisp ]] && syntax=lisp
+	[[ $1 =~ \.el ]] && syntax=lisp
+	[[ $1 =~ \.gdb ]] && syntax=gdb
+	[[ $1 =~ \.GNUmakefile ]] && syntax=makefile
+	[[ $1 =~ \.html ]] && syntax=html
+	[[ $1 =~ \.ini ]] && syntax=ini
+	[[ $1 =~ \.INI ]] && syntax=ini
+	[[ $1 =~ \.java ]] && syntax=java
+	[[ $1 =~ \.json ]] && syntax=json
+	[[ $1 =~ \.js ]]  && syntax=javascript
+	[[ $1 =~ \.lisp ]] && syntax=lisp
+	[[ $1 =~ \.lang ]] && syntax=json
+	[[ $1 =~ \.lua ]] && syntax=lua
+	[[ $1 =~ \.latex ]] && syntax=latex
+	[[ $1 =~ \.less ]] && syntax=less
+	[[ $1 =~ \.markdown ]] && syntax=markdown
+	[[ $1 =~ \.md ]] && syntax=markdown
+	[[ $1 =~ .*\/makefile ]] && syntax=makefile
+	[[ $1 =~ .*\/Makefile ]] && syntax=makefile
+	[[ $1 =~ \.meson ]] && syntax=meson
+	[[ $1 =~ \.objc ]] && syntax=objc
+	[[ $1 =~ \.org ]] && syntax=$HOME/.highlight/org-simple.lang
+#	[[ $1 =~ \.org ]] && syntax=$HOME/.highlight/org.lang
+	[[ $1 =~ \.perl ]] && syntax=perl
+	[[ $1 =~ \.php ]] && syntax=php
+	[[ $1 =~ \.pl ]] && syntax=perl
+	[[ $1 =~ \.py ]] && syntax=python
+	[[ $1 =~ \.qmake ]] && syntax=qmake
+	[[ $1 =~ \.ruby ]] && syntax=ruby
+	[[ $1 =~ \.sh ]] && syntax=bash
+	[[ $1 =~ \.s ]] && syntax=assembler
+	[[ $1 =~ \.tex ]] && syntax=latex
+}
+
+function editopen {
+	if [[ -n $1 ]]
+	then
+		local argument="${1#*:}"
+		local f="${1/:*/}"
+		[[ $f == $argument ]] && argument=
+		[[ $f =~ ^% ]] && f="$(cortex-db -q "$f")"
+	fi
+
+	[[ -z $f ]] && return 2
+	[[ ${f:0:1} != '/' ]] && f="$PWD/$f"
+	local session="$(tmux display-message -p '#S')"
+	[[ -n $2 ]] && session="$2"
+	editwindow "$f" "$argument" "$session"
+	[[ $? == 1 ]] && return 1
+	if [[ -f $f ]]
+	then
+		filename="$f"
+		tmux select-pane -T "$filename"
+		cd "$(dirname $filename)"
+		fileline=1
+		syntax=
+		editsyntax "$filename"
+		[[ -n $argument ]] && editarg "$argument" || editshow $
+	fi
+}
+
+function editclose {
+	[[ -n $filename ]] && filename=
+	[[ -n $fileline ]] && fileline=
+	[[ -n $filesize ]] && filesize=
+	[[ -n $syntax ]] && syntax=
+	[[ -n $fileresult ]] && fileresult=
+	[[ -n $fileresultindex ]] && fileresultindex=
+	tmux select-pane -T "$(hostname)"
+}
+
+function editfind {
+	[[ -z $1 ]] && return 1
+	local result="$(edit "$1")"
+	if [[ -n $result ]]
+	then
+		fileresult=()
+		fileresultindex=-1
+		local IFS=$'\n'
+		local counter=0
+		for i in $result
+		do
+			fileresult+=("${i/$'\t'*/}")
+			printf "$counter:"
+			if [[ -n $syntax ]] && [[ $editsyntax == y ]]
+			then
+				echo "${i/$'\t'/ }" | highlight \
+					--syntax $syntax -s $hitheme -O $himode
+			else
+				echo "${i/$'\t'/ }"
+			fi
+
+			counter=$((counter+1))
+		done
+	fi
+}
+
+function editlocate {
+	[[ -z $1 ]] && return 1
+	local to="$(editsyntax=n edcmd=p \
+		ef "$((fileline + 1)),${filesize}g/$1/n" | head -n1)"
+	to="${to/*:/}"
+	to="${to/\ */}"
+	echo "$to"
+}
+
+function editshow {
+	[[ -n $2 ]] && local filename="$2"
+	[[ -z $filename ]] && return 1
+	[[ ${filename:0:1} != '/' ]] && filename="$PWD/$filename"
+	filesize="$(wc -l "$filename" | cut -d ' ' -f1)"
+	[[ -z $filesize ]] && return 2
+	[[ -z $pagesize ]] && pagesize=20
+	[[ -z $fileline ]] && fileline=1
+	! [[ $fileline =~ [0-9]+ ]] && fileline="$filesize"
+	local arg="$1"
+	[[ -z $1 ]] && arg="+"
+	local show=
+	if [[ -n $fileresult ]] && [[ $fileresultindex ]]
+	then
+		if [[ $arg =~ f([0-9]+) ]]
+		then
+			local n=${arg//f/}
+			if [[ $n -ge 0 ]] && [[ $n -le $((${#fileresult[@]}-1)) ]]
+			then
+				fileresultindex=$n
+				fileline="${fileresult[$fileresultindex]}"
+				printf "$fileresultindex:"
+				editshow ${fileline}
+			else
+				echo "?"
+			fi
+
+			return
+		elif [[ $arg == "u" ]]
+		then
+			if [[ $fileresultindex -eq 0 ]] || [[ $fileresultindex -eq -1 ]]
+			then
+				fileresultindex=$((${#fileresult[@]}-1))
+			elif [[ $fileresultindex -gt 0 ]]
+			then
+				fileresultindex=$((fileresultindex-1))
+			fi
+
+			fileline="${fileresult[$fileresultindex]}"
+			printf "$fileresultindex:"
+			editshow $fileline
+			return
+		elif [[ $arg == "d" ]]
+		then
+			if [[ $fileresultindex -eq $((${#fileresult[@]}-1)) ]]
+			then
+				fileresultindex=0
+			elif [[ $fileresultindex -lt $((${#fileresult[@]}-1)) ]]
+			then
+				fileresultindex=$((fileresultindex+1))
+			fi
+
+			fileline="${fileresult[$fileresultindex]}"
+			printf "$fileresultindex:"
+			editshow $fileline
+			return
+		elif [[ $arg == "s" ]]
+		then
+			local counter=0
+			for i in "${fileresult[@]}"
+			do
+				local line="$(edit ${i}n)"
+				line="${line/$'\t'/ }"
+				printf "$counter:"
+				if [[ -n $syntax ]] && [[ $editsyntax == y ]]
+				then
+					echo "$line" | highlight \
+						--syntax $syntax -s $hitheme -O $himode
+				else
+					echo "$line"
+				fi
+
+				counter=$((counter+1))
+			done
+
+			return
+		elif [[ $arg == "m" ]]
+		then
+			local asize=${#fileresult[@]}
+			if [[ $fileresultindex -lt $((asize - 1)) ]]
+			then
+				local start="${fileresult[$fileresultindex]}"
+				local end="$((${fileresult[$((fileresultindex + 1))]} - 1))"
+				fileline="$end"
+				editshow ${start},${end}
+			else
+				editshow c
+			fi
+
+			return
+		fi
+	fi
+
+	if [[ $arg == "r" ]]
+	then
+		if [[ -n $eslast ]] && [[ ${eslast:0-1} != $edcmd ]]
+		then
+			[[ ${eslast:0-1} == "p" ]] \
+				&& eslast="${eslast/%p/n}" \
+				|| eslast="${eslast/%n/p}"
+		fi
+
+		show="$eslast"
+	elif [[ $arg == "$" ]] || [[ $arg == "G" ]]
+	then
+		fileline="$filesize"
+		show="edit ${fileline}$edcmd"
+	elif [[ $arg == "g" ]]
+	then
+		fileline="1"
+		show="edit ${fileline}$edcmd"
+	elif [[ $arg =~ ^([.+-]?([0-9]+)?)(,[$+]?([0-9]+)?)?$ ]] \
+		&& [[ $arg != "." ]]
+	then
+		[[ $arg == "+" ]] && arg="$((fileline + 1))"
+		[[ $arg == "-" ]] && arg="$((fileline - 1))"
+		if [[ $arg =~ , ]]
+		then
+			local head="${arg/,*/}"
+			local tail="${arg/*,/}"
+			[[ $head == "." ]] && head="$fileline"
+			[[ $head =~ ^\-[0-9]+ ]] && head="${head/-/}" \
+				&& head="$((fileline - head))"
+			[[ $head =~ ^\+[0-9]+ ]] && head="${head/+/}" \
+				&& head="$((fileline + head))"
+			[[ $head -lt 1 ]] && head="1"
+			[[ $tail == "$" ]] && tail="$filesize"
+			[[ $tail =~ ^\+[0-9]+ ]] && tail="${tail/+/}" \
+				&& tail="$((fileline + tail))"
+			[[ $tail -gt $filesize ]] && tail="$filesize"
+			show="edit ${head},${tail}$edcmd"
+			fileline="$tail"
+		else
+			[[ $arg =~ ^\+[0-9]+ ]] && arg="${arg/+/}" \
+				&& arg="$((fileline + arg))"
+			[[ $arg =~ ^\-[0-9]+ ]] && arg="${arg/-/}" \
+				&& arg="$((fileline - arg))"
+			[[ $arg -lt 1 ]] && arg="1"
+			[[ $arg -gt $filesize ]] && arg="$filesize"
+			show="edit ${arg}$edcmd"
+			fileline="$arg"
+		fi
+	elif [[ $arg == "l" ]] || [[ $arg == "." ]]
+	then
+		show="edit ${fileline}$edcmd"
+	elif [[ $pagesize -ge $filesize ]]
+	then
+		show="edit "1,${filesize}$edcmd""
+	elif [[ $arg == "n" ]]
+	then
+		[[ $eslastarg == "p" ]] \
+			&& fileline="$((fileline + pagesize + 2))"
+		if [[ $fileline -ge $((filesize - pagesize)) ]]
+		then
+			show="edit "$((filesize - pagesize)),${filesize}$edcmd""
+			fileline="$filesize"
+		else
+			show="edit "${fileline},$((fileline + pagesize))$edcmd""
+			fileline="$((fileline + pagesize + 1))"
+		fi
+	elif [[ $arg == "p" ]]
+	then
+		[[ $eslastarg == "n" ]] \
+			&& [[ $fileline -ne $filesize ]] \
+			&& fileline="$((fileline - pagesize - 2))"
+		[[ $eslastarg == "n" ]] \
+			&& [[ $fileline == $filesize ]] \
+			&& fileline="$((fileline - pagesize - 1))"
+		if [[ $fileline -le $pagesize ]]
+		then
+			show="edit "1,${pagesize}$edcmd""
+			fileline=$pagesize
+		else
+			show="edit "$((fileline - pagesize)),${fileline}$edcmd""
+			fileline=$((fileline - pagesize - 1))
+		fi
+	elif [[ $arg == "b" ]]
+	then
+		show="edit "1,${pagesize}$edcmd""
+		fileline=$((pagesize + 1))
+	elif [[ $arg == "e" ]]
+	then
+		show="edit "$((filesize - pagesize)),${filesize}$edcmd""
+		fileline=$((filesize - pagesize - 1))
+	elif [[ $arg == "a" ]]
+	then
+		show="edit ,$edcmd"
+	elif [[ $arg == "c" ]]
+	then
+		local head="$((fileline - (pagesize / 2)))"
+		local tail="$((fileline + (pagesize / 2)))"
+		[[ $head -lt 1 ]] && head="1" \
+			&& tail="$((tail + (pagesize / 2)))"
+		[[ $tail -gt $filesize ]] && tail="$filesize"
+		show="edit ${head},${tail}$edcmd"
+	elif [[ $arg =~ ^\/.*(,\/.*)? ]] && [[ -z $show ]]
+	then
+		if [[ $arg =~ , ]]
+		then
+			local head="${arg/,*/}"
+			local tail="${arg/*,/}"
+			head="$(editlocate "${head/\//}")"
+			tail="$(editlocate "${tail/\//}")"
+			[[ -n $head ]] && [[ -n $tail ]] \
+				&& show="edit ${head},${tail}$edcmd" \
+				&& fileline="$tail"
+		else
+			local line="$(editlocate "${arg/\//}")"
+			[[ $line =~ ^[0-9]+$ ]] && show="edit ${line}$edcmd" \
+				&& fileline="$line"
+		fi
+	fi
+
+	if [[ -n $show ]]
+	then
+		if [[ -n $syntax ]] && [[ $editsyntax == y ]]
+		then
+			$show | highlight --syntax $syntax -s $hitheme -O $himode
+		else
+			$show
+		fi
+
+		eslastarg="$arg"
+		eslast="$show"
+	fi
+}
+
+function editappend {
+	edit "${fileline}a\n$1\n.\nw"
+	[[ -n $2 ]] && editshow "+$2" \
+		|| editshow "+$(echo -e "$1" | grep -c "^")"
+}
+
+function editinsert {
+	fileline="$((fileline - 1))"
+	editappend "$1" "$2"
+}
+
+function editdelete {
+	[[ -n $1 ]] && local to="$1"
+	[[ $1 =~ ^\+[0-9]+ ]] && to="${1/\+/}" && to="$((fileline + to))"
+	[[ $to -gt $filesize ]] && return 1
+	[[ -z $to ]] && edit "${fileline}d\nw" || edit "${fileline},${to}d\nw"
+	filesize="$(wc -l "$filename" | cut -d ' ' -f1)"
+	[[ $fileline -gt $filesize ]] && fileline="$filesize"
+}
+
+function editchange {
+	if [[ -n $1 ]]
+	then
+		[[ -n $2 ]] && local to="$2"
+		[[ $2 =~ ^\+[0-9]+ ]] && to="${2/\+/}" && to="$((fileline + to))"
+		[[ $to -gt $filesize ]] && return 1
+		[[ -z $to ]] && edit "${fileline}c\n$1\n.\nw" \
+			|| edit "${fileline},${to}c\n$1\n.\nw"
+		[[ -z $to ]] && editshow l || editshow ${fileline},$to
+	else
+		return 2
+	fi
+}
+
+function editsub {
+	if [[ -n $1 ]]
+	then
+		[[ -n $3 ]] && local to="$3"
+		[[ $to =~ ^\+[0-9]+ ]] && to="${to/\+/}" && to="$((fileline + to))"
+		[[ $to == '%' ]] && to="$filesize"
+		[[ $to -gt $filesize ]] && return 1
+		if [[ -z $to ]] || [[ $to == " " ]]
+		then
+			local pattern="s/$1/$2/"
+			[[ $1 =~ $'\\'$ ]] && pattern="s/$1\/$2/"
+			[[ $2 =~ $'\\'$ ]] && pattern="s/$1/$2\/"
+			[[ $4 == "g" ]] && pattern="${pattern}g"
+			edit "$fileline$pattern\nw"
+		else
+			local pattern="s/$1/$2/"
+			local lines="${fileline},${to}"
+			[[ $1 =~ $'\\'$ ]] && pattern="s/$1\/$2/"
+			[[ $2 =~ $'\\'$ ]] && pattern="s/$1/$2\/"
+			[[ $3 == "%" ]] && lines="1,${filesize}"
+			[[ $4 == "g" ]] && pattern="${pattern}g"
+			edit "$lines$pattern\nw"
+		fi
+
+		editshow l
+	else
+		return 2
+	fi
+}
+
+function editjoin {
+	local l=
+	[[ -z $1 ]] && l="$((fileline + 1))"
+	[[ $1 =~ ^\+[0-9]+ ]] && l="${1/\+/}" && l="$((fileline + l))"
+	[[ $l -gt $filesize ]] && return 1
+	[[ -n $l ]] && edit "${fileline},${l}j\nw" && editshow ${fileline}
+}
+
+function editmove {
+	local dest=
+	[[ -z $1 ]] && dest="$((fileline + 1))"
+	[[ $1 =~ ^\+[0-9]+ ]] && dest="${1/\+/}" && dest="$((fileline + dest))"
+	[[ $1 =~ ^\-[0-9]+ ]] && dest="${1/\-/}" && dest="$((fileline - dest))"
+	[[ $dest -gt $filesize ]] && return 1
+	[[ $dest -lt 1 ]] && return 1
+	local to=
+	[[ $2 =~ ^\+[0-9]+ ]] && to="${dest/\+/}" && to="$((fileline + l))"
+	[[ -n $to ]] && edit "${fileline},${to}m$dest\nw" \
+			|| edit "${fileline}m$dest\nw"
+}
+
+function edittransfer {
+	yank=
+	local line="$1"
+	[[ $1 == "." ]] && line="$fileline"
+	[[ -n $2 ]] && local n="$2" \
+		&& [[ $n =~ ^\+ ]] && n="${n/\+/}" && n="$((fileline + n))"
+	if [[ $line -gt 0 ]]
+	then
+		[[ -n $n ]] && edit "${fileline},${n}t$line\nw" \
+			|| edit "${fileline}t$line\nw"
+	elif [[ $1 -eq 0 ]] && [[ -n $n ]]
+	then
+		yank="$(edcmd=p editsyntax=n editshow ${fileline},$n)"
+	else
+		yank="$(edcmd=p editsyntax=n editshow l)"
+	fi
+
+	[[ $3 == "x" ]] && [[ -n $yank ]] && echo "$yank" | xclip -i
+}
+
+function editlevel {
+	local line=
+	[[ -n $1 ]] \
+		&& line="$(editsyntax=n edcmd=p es $1)" \
+		|| line="$(editsyntax=n edcmd=p es l)"
+	[[ -n $line ]] && echo "$line" | awk -F '\t' '{ print NF-1 }' || return 1
+}
+
+function editspaces {
+	local n=0
+	local line=
+	[[ -n $1 ]] \
+		&& line="$(editsyntax=n edcmd=p es $1)" \
+		|| line="$(editsyntax=n edcmd=p es l)"
+	[[ -z $line ]] && return 1
+	while true
+	do
+		if [[ $line =~ ^\  ]]
+		then
+			n="$((n + 1))"
+			line="${line/ /}"
+		else
+			break	
+		fi
+	done
+
+	echo "$n"
+}
+
+function ea { editappend "$@"; }
+function ech { editchange "$@"; }
+function ec { editcmd "$@"; }
+function edel { editdelete "$@"; }
+function ef { editfind "$@"; }
+function ei { editinsert "$@"; }
+function ej { editjoin "$@"; }
+function ek { editclose "$@"; }
+function els { editspaces "$@"; }
+function el { editlevel "$@"; }
+function em { editmove "$@"; }
+function eo { editopen "$@"; }
+function er { editread "$@"; }
+function esu { editsub "$@"; }
+function es { editshow "$@"; }
+function et { editstore "$@"; }
+function eu { editundo "$@"; }
+function ey { edittransfer "$@"; }
+function e { edit "$@"; }
